81d0b87f5e59badf63288178a766d95f4b10f103
AW001TI <India@4a>
2018-09-19 18:06:22 +0530

    Added test cases for deleting / updating elements in single linked list.

--- src/test/java/ds/list/SingleLinkedListTest.java
+++ src/test/java/ds/list/SingleLinkedListTest.java
@@ -1,5 +1,6 @@
 package ds.list;
 
+import ds.common.DSException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -55,5 +56,60 @@ public final class SingleLinkedListTest {
         Assert.assertEquals("first" , list.getElements().get(0));
         Assert.assertEquals("fifth" , list.getElements().get(4));
         Assert.assertEquals(true , list.findElement("third"));
+    }
+
+    @Test
+    public void addFunctionalityTest(){
+        IList<String> list = new SingleLinkedList<String>();
+        list.add("first");
+        list.add("second");
+        Assert.assertEquals(2 , list.getNumberOfElements());
+        list.add("third");
+        Assert.assertEquals(3 , list.getNumberOfElements());
+    }
+
+    @Test
+    public void addAfterBeforeFindTest(){
+        IList<String> list = new SingleLinkedList<String>();
+        list.add("first");
+        list.add("second");
+        try {
+            list.addBefore("second", "third");
+            Assert.assertEquals("third" , list.getElements().get(1));
+            Assert.assertEquals(3 , list.getNumberOfElements());
+            list.addAfter("second" , "fourth");
+            Assert.assertEquals(4 , list.getNumberOfElements());
+            Assert.assertEquals("fourth" , list.getElements().get(3));
+        }catch (DSException ex){
+            Assert.fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void addAfterBeforeExceptionTest(){
+        IList<String> list = new SingleLinkedList<String>();
+        list.add("first");
+        list.add("second");
+        try{
+            list.addAfter("third" , "fourth");
+        }catch (DSException ex){
+            Assert.assertEquals(DSException.LIST_ELEMENT_NOT_FOUND_STR , ex.getMessage());
+        }
+
+        try{
+            list.addBefore("third" , "fourth");
+        }catch (DSException ex){
+            Assert.assertEquals(DSException.LIST_ELEMENT_NOT_FOUND_STR , ex.getMessage());
+        }
+    }
+
+    @Test
+    public void findElementInSingleLinkedListTest(){
+        IList<String> list = new SingleLinkedList<String>();
+        list.add("first");
+        list.add("second");
+
+        Assert.assertEquals(true , list.findElement("first"));
+        Assert.assertEquals(false , list.findElement("fourth"));
     }
 }
